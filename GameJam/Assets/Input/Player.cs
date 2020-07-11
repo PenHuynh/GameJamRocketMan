33a4c0de7a5d47a11d4212141fc80c826f3e8ef8
Anton <huber.anton@hotmail.com>
2020-07-11 12:32:55 +1000

    Basic ball controls added. Camera follow added

--- GameJam/Assets/Input/Player.cs
+++ GameJam/Assets/Input/Player.cs
@@ -4,44 +4,47 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public Rigidbody2D m_rigidbody2D;
+    private Rigidbody2D m_rigidbody2D;
     public float m_jumpForce;
     public float m_fallForce;
     public float m_moveSpeed;
 
+    public GameObject m_model;
+    public Camera m_camera;
+
     // Start is called before the first frame update
     void Start()
     {
-        m_rigidbody2D = this.GetComponent<Rigidbody2D>();
+
+        m_rigidbody2D = m_model.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-       
-
+        
         if(Input.GetKey(KeyCode.RightArrow))
         {
-            m_rigidbody2D.AddForce((transform.right * m_moveSpeed), ForceMode2D.Impulse);
+            m_rigidbody2D.AddForce((Vector3.right * m_moveSpeed) * Time.deltaTime, ForceMode2D.Impulse);
         }
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            m_rigidbody2D.AddForce((transform.right * -1) * m_moveSpeed, ForceMode2D.Impulse);
+            m_rigidbody2D.AddForce((Vector3.right * -1) * m_moveSpeed * Time.deltaTime, ForceMode2D.Impulse);
         }
 
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            m_rigidbody2D.AddForce((transform.up * -1) * m_fallForce, ForceMode2D.Impulse);
+            m_rigidbody2D.AddForce((Vector3.up * -1) * m_fallForce * Time.deltaTime, ForceMode2D.Impulse);
         }
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            m_rigidbody2D.AddForce((transform.up * m_jumpForce), ForceMode2D.Impulse);
+            m_rigidbody2D.AddForce((transform.up * m_jumpForce) * Time.deltaTime, ForceMode2D.Impulse);
         }
-        
 
-
+        Vector3 cameraPos = new Vector3(m_model.transform.position.x, m_model.transform.position.y, m_camera.transform.position.z);
+        m_camera.transform.position = cameraPos;
 
     }
 }
---
+++ GameJam/Assets/Input/RocketRotation.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketRotation : MonoBehaviour
+{
+
+    public GameObject m_roationPoint;
+    public float m_rotationSpeed;
+    // The arm will try to be this far away from the 
+    // rotaiton point at all times.
+    public float m_armMaxRange;
+
+    // Since the rocket is buried in the hierarchy.
+    public GameObject m_rocket;
+    private Rigidbody m_rocketRigidBody;
+
+    public float m_rocketForce = 3;
+    private void Start()
+    {
+        m_rocketRigidBody = m_rocket.GetComponent<Rigidbody>();
+    }
+
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Extend arm out max range.
+
+
+
+
+
+        // Left
+        if(Input.GetKey(KeyCode.A))
+        {
+
+        }
+        // Right
+        if(Input.GetKey(KeyCode.D))
+        {
+
+        }
+
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            m_rocketRigidBody.AddForce((m_rocket.transform.up * m_rocketForce), ForceMode.Impulse);
+        }
+    }
+}
